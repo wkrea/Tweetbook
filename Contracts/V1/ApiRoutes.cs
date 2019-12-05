cf279fe877920d759f6c545b7c2fb3c242b549f5
wkrea <william.trigos@gmail.com>
2019-12-05 10:42:22 -0500

    Versionamiento de Api Contracts  Fin del video 2 https://www.youtube.com/watch?v=WFEE5yVJwGU&list=PLUOequmGnXxOgmSDWU7Tl6iQTsOtyjtwU&index=2&t=317s

---
+++ Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,17 @@
+﻿namespace Tweetbook.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+
+        public const string Base = Root + "/" + Version;
+
+        public static class Posts
+        {
+            public const string GetAll = Base + "/posts";
+            //public static readonly string GetById = $"{Base}/posts/{PostId}";
+            //public static readonly string Create  = $"{Base}/posts";
+        }
+    }
+}
