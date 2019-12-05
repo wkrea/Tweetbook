553f0f03cca0cee0ed39936d8007c22700e5126f
wkrea <william.trigos@gmail.com>
2019-12-05 14:24:48 -0500

    * Creando Interfaz para instalación limpia de los
    servicios en StartUp según el video
    https://www.youtube.com/watch?v=ESdvXlrG9zQ&list=PLUOequmGnXxOgmSDWU7Tl6iQTsOtyjtwU&index=3&t=415s
    
    * Reparando Bug para Swagger en versión NetCore 3.0 según sugerencias
    de
    https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/982#issuecomment-486752000

---
+++ Installers/DatabaseInstaller.cs
@@ -0,0 +1,22 @@
+﻿using kreaCore.Installers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tweetbook.Data;
+
+namespace Tweetbook.Installers
+{
+    public class DatabaseInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(
+                options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>(
+                options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<DataContext>();
+        }
+    }
+}
---
+++ Installers/MvcInstaller.cs
@@ -0,0 +1,23 @@
+﻿using kreaCore.Installers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Tweetbook.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddMvc();
+
+            // https://stackoverflow.com/questions/58362757/could-not-load-type-microsoft-aspnetcore-mvc-mvcjsonoptions-from-assembly-mi
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Tweetbook API", Version = "v1" });
+            });
+        }
+    }
+}
