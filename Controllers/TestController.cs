7f71609f656e97d9033525f6666d3e8d04644da2
wkrea <william.trigos@gmail.com>
2019-12-04 20:57:49 -0500

    Instalación y Prueba de Swagger
    Fin del video
    https://www.youtube.com/watch?v=sdlt3-ptt9g&list=PLUOequmGnXxOgmSDWU7Tl6iQTsOtyjtwU&index=1&t=275s

---
+++ Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Tweetbook.Controllers
+{
+    public class TestController : Controller
+    {
+        public TestController()
+        {
+            //List<string> Posts = new List<string>();
+
+            //foreach(var i )
+            //{
+
+            //}
+        }
+
+        [HttpGet("api/user")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Nick" });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Tweetbook.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using kreaCore.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace Tweetbook
 {
@@ -32,8 +29,17 @@ namespace Tweetbook
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<DataContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddMvc();
+
+            // https://stackoverflow.com/questions/58362757/could-not-load-type-microsoft-aspnetcore-mvc-mvcjsonoptions-from-assembly-mi
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Tweetbook API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,12 +51,21 @@ namespace Tweetbook
             }
             else
             {
-               app.UseHsts();
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            //app.UseRouting();
+
+            var swaggerOptions = new SwaggerOpts();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+            app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute; });
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
+
+            app.UseRouting();
 
             //app.UseAuthentication();
             //app.UseAuthorization();
